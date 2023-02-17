1ec6e8def7fa4b251428a223d7a7bb64ff578426
esimed <contact@esimed.fr>
2023-02-17 15:53:45 +0100

    initial import

---
+++ app/services/baseapiservice.js
@@ -0,0 +1,12 @@
+const serviceBaseUrl = "http://XX.XX.XX.XX:3333"
+
+class BaseAPIService {
+    constructor(url) {
+        this.url = `${serviceBaseUrl}/${url}`
+        this.token = sessionStorage.getItem("token")
+        this.headers = new Headers()
+        if (this.token !== undefined) {
+            this.headers.append("Authorization", `Bearer ${this.token}`)
+        }
+    }
+}
